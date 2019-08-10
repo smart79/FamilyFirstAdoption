f7175559dd0db93655415cba56125b1ea5942fe6
Khurana <39412425+Ashish0125@users.noreply.github.com>
2019-08-10 10:47:36 -0500

    added config
    test branch to ashish

--- models/children.js
+++ models/children.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
         }
       },
       Cage: { 
-          type: DataTypes.INT,
+          type: DataTypes.INTEGER,
           allowNull: false,
         },
       Ccity: {
