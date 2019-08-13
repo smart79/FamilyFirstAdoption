e32a75dd73e24aa26fb140575ea372820c49e8a6
Chris Martinez <smartinez79@protonmail.com>
2019-08-13 03:22:10 -0500

    update to survey-form

--- client/src/Components/toolbar/navMenu.js
+++ client/src/Components/toolbar/navMenu.js
@@ -22,7 +22,7 @@ class NavMenu extends React.Component {
                         </div>
                         <nav className="navbar navbar-expand-lg navbar-light bg-light">
                             <div className="container">
-                                <a className="navbar-brand" href="/">Adopted</a>
+                                <a className="navbar-brand" href="/">Family First Adoption Services</a>
                                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
@@ -43,7 +43,7 @@ class NavMenu extends React.Component {
                                         </li>
 
                                         <li className="nav-item">
-                                            <a className="nav-link" href="/About">About</a>
+                                            <a className="nav-link" href="./About">About</a>
                                         </li>
                                         <li className="nav-item">
                                             <a className="nav-link" href="contact.html">Contact</a>
