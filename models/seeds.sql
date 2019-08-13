bbf32c09d1f7e0dc7e11912af5a1692d451b5064
Chris Martinez <smartinez79@protonmail.com>
2019-08-13 01:56:31 -0500

    update to survey

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO Sponsor
-    (SfirstName, SlastName, Semail, Sphone, Scity, Sstate, Scountry)
+    (SfirstName, SlastName, Semail, Sphone, Scity, Sstate, Scountry, Simage)
 VALUES
     ('Ashish', 'Singh', 'ashishkhurana17@gmail.com', 4694862840, 'Lewisville', "TX", 'USA');
 
 INSERT INTO Children
-    (CfirstName, Cimage, ClastName, Cage, Ccity, Cstate, Ccountry)
+    (CfirstName, Cimage, ClastName, Cage, CGender, Ccity, Cstate, Ccountry, Cspnds)
 VALUES
-    ('Mike', "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg", "Doe", 8, "Lewisville", "TX", "USA")
+    ('Mike', "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg", "Doe", 8, "male", "Lewisville", "TX", "USA")
