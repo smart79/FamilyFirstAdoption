f002228288306b58fea41eade300e6a861b5638b
Chris Martinez <smartinez79@protonmail.com>
2019-08-13 20:36:59 -0500

    update to survey

--- client/src/Components/toolbar/survey-form.js
+++ client/src/Components/toolbar/survey-form.js
@@ -3,9 +3,35 @@ import React, { Component } from 'react';
 // import {Route, Link, BrowserRouter as Router} from 'react-router-dom';
 
 class Survey extends Component {
-  handleInputs = () => {
-    console.log("works")
+  state = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    spFname: "",
+    spLname: "",
+    city: "",
+    state: "",
+    country: "",
+    phone: "",
+    email: "",
+    religion: "",
+    occupation: "",
+    education: "",
+    photo: "",
+    adoptC: "",
+    adoptG: "",
+    adoptA: "",
+    adoptS: "",
   }
+
+  handleInputs = (event) => {
+    console.log(event.target)
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div className="About">
@@ -15,51 +41,293 @@ class Survey extends Component {
           <h3><strong>About You</strong></h3>
 
           <h4>First Name (Required)</h4>
-          <input type="text" id="Fname" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="Fname" name="firstName" value={this.state.firstName} className="form-control" onChange={this.handleInputs} required />
           <h4>Last Name (Required)</h4>
-          <input type="text" id="Lname" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="Lname" name="lastName" value={this.state.lastName} className="form-control" onChange={this.handleInputs} required />
           <h4>Address</h4>
-          <input type="text" id="address" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="address" name="address" value={this.state.address} className="form-control" onChange={this.handleInputs} />
           <h4>Spouse's First Name</h4>
-          <input type="text" id="spouseFname" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="spouseFname" name="spFname" value={this.state.spFname} className="form-control" onChange={this.handleInputs} />
           <h4>Spouse's Last Name</h4>
-          <input type="text" id="spouseLname" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="spouseLname" name="spLname" value={this.state.spLname} className="form-control" onChange={this.handleInputs} />
           <h4>Address</h4>
-          <input type="text" id="address" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="spaddress" name="spaddress" value={this.state.spaddress} className="form-control" onChange={this.handleInputs} />
           <h4>City (Required)</h4>
-          <input type="text" id="city" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="city" name="city" value={this.state.city} className="form-control" onChange={this.handleInputs} required />
           <h4>State (Required)</h4>
-          <input type="text" id="state" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="state" name="state" value={this.state.state} className="form-control" onChange={this.handleInputs} required />
           <h4>Country (Required)</h4>
-          <input type="text" id="state" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="country" name="country" value={this.state.country} className="form-control" onChange={this.handleInputs} required />
           <h4>Phone Number (Required)</h4>
-          <input type="text" id="phone" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="phone" name="phone" value={this.state.phone} className="form-control" onChange={this.handleInputs} required />
           <h4>Email (Required)</h4>
-          <input type="text" id="email" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="email" name="email" value={this.state.email} className="form-control" onChange={this.handleInputs} required />
           <h4>Religion</h4>
-          <input type="text" id="religion" className="form-control" onChange={this.handleInputs} />
-          <h4>Ocupation</h4>
-          <input type="text" id="ocupation" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="religion" name="religion" value={this.state.religion} className="form-control" onChange={this.handleInputs} />
+          <h4>Occupation</h4>
+          <input type="text" id="occupation" name="occupation" value={this.state.occupation} className="form-control" onChange={this.handleInputs} />
           <h4>Education</h4>
-          <input type="text" id="education" className="form-control" onChange={this.handleInputs} />
+          <input type="text" id="education" name="education" value={this.state.education} className="form-control" onChange={this.handleInputs} />
 
           <h4>Link to Photo Image (Required)</h4>
-          <input type="text" id="photo" className="form-control" onChange={this.handleInputs} required />
+          <input type="text" id="photo" name="photo" value={this.state.photo} className="form-control" onChange={this.handleInputs} required />
 
           <hr />
 
           <h3><strong>Question 1</strong></h3>
           <h4>What Country do you wish to Adopt from?</h4>
-          <select className="chosen-select" id="q1">
+          <select className="chosen-select" id="q1" name="adoptC" value={this.state.adoptC} onChange={this.handleInputs} >
             <option value=""></option>
-            <option value="1">____</option>
-            <option value="2">____</option>
-            <option value="3">No preference</option>>
+            <option value="1">Afghanistan</option>
+            <option value="2">Albania</option>
+            <option value="3">Algeria</option>
+            <option value="4">American Samoa</option>                  
+                              Andorra
+                              Angola
+                              Anguilla
+                              Antigua and Barbuda
+                              Argentina
+                              Armenia
+                              Aruba
+                              Australia
+                              Austria
+                              Azerbaijan
+                              The Bahamas
+                              Bahrain
+                              Bangladesh
+                              Barbados
+                              Belarus
+                              Belgium
+                              Belize
+                              Benin
+                              Bermuda
+                              Bhutan
+                              Bolivia
+                              Bosnia and Herzegovina
+                              Botswana
+                              Brazil
+                              Brunei
+                              Bulgaria
+                              Burkina Faso
+                              Burundi
+                              Cambodia
+                              Cameroon
+                              Canada
+                              Cape Verde
+                              Cayman Islands
+                              Central African Republic
+                              Chad     
+                              Chile
+                              People 's Republic of China
+                              Republic of China
+                              Christmas Island
+                              Cocos(Keeling) Islands
+                              Colombia
+                              Comoros
+                              Congo
+                              Cook Islands
+                              Costa Rica
+                              Cote d'Ivoire
+                              Croatia
+                              Cuba
+                              Cyprus
+                              Czech Republic
+                              Denmark
+                              Djibouti
+                              Dominica
+                              Dominican Republic
+                              Ecuador
+                              Egypt
+                              El Salvador
+                              Equatorial Guinea
+                              Eritrea
+                              Estonia
+                              Ethiopia
+                              Falkland Islands
+                              Faroe Islands
+                              Fiji
+                              Finland
+                              France
+                              French Polynesia
+                              Gabon
+                              The Gambia
+                              Georgia
+                              Germany
+                              Ghana
+                              Gibraltar
+                              Greece
+                              Greenland
+                              Grenada
+                              Guadeloupe
+                              Guam
+                              Guatemala
+                              Guernsey
+                              Guinea
+                              Guinea - Bissau
+                              Guyana
+                              Haiti
+                              Honduras
+                              Hong Kong
+                              Hungary
+                              Iceland
+                              India
+                              Indonesia
+                              Iran
+                              Iraq
+                              Ireland
+                              Israel
+                              Italy
+                              Jamaica
+                              Japan
+                              Jersey
+                              Jordan
+                              Kazakhstan
+                              Kenya
+                              Kiribati
+                              North Korea
+                              South Korea
+                              Kosovo
+                              Kuwait
+                              Kyrgyzstan
+                              Laos
+                              Latvia
+                              Lebanon
+                              Lesotho
+                              Liberia
+                              Libya
+                              Liechtenstein
+                              Lithuania
+                              Luxembourg
+                              Macau
+                              Macedonia
+                              Madagascar
+                              Malawi
+                              Malaysia
+                              Maldives
+                              Mali
+                              Malta
+                              Marshall Islands
+                              Martinique
+                              Mauritania
+                              Mauritius
+                              Mayotte
+                              Mexico
+                              Micronesia
+                              Moldova
+                              Monaco
+                              Mongolia
+                              Montenegro
+                              Montserrat
+                              Morocco
+                              Mozambique
+                              Myanmar
+                              Nagorno - Karabakh
+                              Namibia
+                              Nauru
+                              Nepal
+                              Netherlands
+                              Netherlands Antilles
+                              New Caledonia
+                              New Zealand
+                              Nicaragua
+                              Niger
+                              Nigeria
+                              Niue
+                              Norfolk Island
+                              Turkish Republic of Northern Cyprus
+                              Northern Mariana
+                              Norway
+                              Oman
+                              Pakistan
+                              Palau
+                              Palestine
+                              Panama
+                              Papua New Guinea
+                              Paraguay
+                              Peru
+                              Philippines
+                              Pitcairn Islands
+                              Poland
+                              Portugal
+                              Puerto Rico
+                              Qatar
+                              Romania
+                              Russia
+                              Rwanda
+                              Saint Barthelemy
+                              Saint Helena
+                              Saint Kitts and Nevis
+                              Saint Lucia
+                              Saint Martin
+                              Saint Pierre and Miquelon
+                              Saint Vincent and the Grenadines
+                              Samoa
+                              San Marino
+                              Sao Tome and Principe
+                              Saudi Arabia
+                              Senegal
+                              Serbia
+                              Seychelles
+                              Sierra Leone
+                              Singapore
+                              Slovakia
+                              Slovenia
+                              Solomon Islands
+                              Somalia
+                              Somaliland
+                              South Africa
+                              South Ossetia
+                              Spain
+                              Sri Lanka
+                              Sudan
+                              Suriname
+                              Svalbard
+                              Swaziland
+                              Sweden
+                              Switzerland
+                              Syria
+                              Taiwan
+                              Tajikistan
+                              Tanzania
+                              Thailand
+                              Timor - Leste
+                              Togo
+                              Tokelau
+                              Tonga
+                              Transnistria Pridnestrovie
+                              Trinidad and Tobago
+                              Tristan da Cunha
+                              Tunisia
+                              Turkey
+                              Turkmenistan
+                              Turks and Caicos Islands
+                              Tuvalu
+                              Uganda
+                              Ukraine
+                              United Arab Emirates
+                              United Kingdom
+            <option value="4">United States</option>
+                              Uruguay
+                              Uzbekistan
+                              Vanuatu
+                              Vatican City
+                              Venezuela
+                              Vietnam
+                              British Virgin Islands
+                              Isle of Man
+                              US Virgin Islands
+                              Wallis and Futuna
+                              Western Sahara
+                              Yemen
+                              Zambia
+                              Zimbabwe
+            
+            <option value="5">No preference</option>>
           </select>
 
           <h3><strong>Question 2</strong></h3>
           <h4>Do you have a gender preference when adopting? </h4>
-          <select className="chosen-select" id="q2">
+          <select className="chosen-select" id="q2" name="adoptG" value={this.state.adoptG} onChange={this.handleInputs}>
             <option value=""></option>
             <option value="1">Male</option>
             <option value="2">Female</option>
@@ -68,7 +336,7 @@ class Survey extends Component {
 
           <h3><strong>Question 3</strong></h3>
           <h4>What age Group do you prefer to adopt?</h4>
-          <select className="chosen-select" id="q2">
+          <select className="chosen-select" id="q2" name="adoptA" value={this.state.adoptA} onChange={this.handleInputs}>
             <option value=""></option>
             <option value="1">under 5 years</option>
             <option value="2">over 5 years old</option>
@@ -76,8 +344,8 @@ class Survey extends Component {
           </select>
 
           <h3><strong>Question 4</strong></h3>
-          <h4>Will you concider Special needs when adopting? </h4>
-          <select className="chosen-select" id="q2">
+          <h4>Will you concider Special needs when adopting?</h4>
+          <select className="chosen-select" id="q2" name="adoptS" value={this.state.adoptS} onChange={this.handleInputs}>
             <option value=""></option>
             <option value="1">Yes</option>
             <option value="2">No</option>
