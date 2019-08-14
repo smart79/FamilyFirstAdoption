8d27231577720ad5e0b832db15842c0bd5159e80
Chris Martinez <smartinez79@protonmail.com>
2019-08-14 01:24:55 -0500

    import toolbar.css

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO Sponsor
-    (SfirstName, SlastName, Semail, Sphone, Scity, Sstate, Scountry, Simage)
+    (id, SfirstName, SlastName, Semail, Sphone, Scity, Sstate, Scountry, Simage)
 VALUES
-    ('Ashish', 'Singh', 'ashishkhurana17@gmail.com', 4694862840, 'Lewisville', "TX", 'USA');
+    (1, 'Ashish', 'Singh', 'ashishkhurana17@gmail.com', 4694862840, 'Lewisville', "TX", 'USA');
 
 INSERT INTO Children
-    (CfirstName, Cimage, ClastName, Cage, CGender, Ccity, Cstate, Ccountry, Cspnds)
+    (id, CfirstName, Cimage, ClastName, Cage, Cgender, Ccity, Cstate, Ccountry, Cspnds)
 VALUES
-    ('Mike', "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg", "Doe", 8, "male", "Lewisville", "TX", "USA")
+    (1, 'Mike', "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg", "Doe", 8, "male", "Lewisville", "TX", "USA", "yes")
