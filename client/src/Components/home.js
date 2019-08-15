ca1814380a4fc984b5d1f1b80ef458dc16a5bc8a
Chris Martinez <smartinez79@protonmail.com>
2019-08-14 22:35:57 -0500

    added contact.js and changes to nav menu and survey.js

--- client/src/Components/home.js
+++ client/src/Components/home.js
@@ -3,12 +3,13 @@ import './toolbar/toolbar.css';
 import Jumbotron from './toolbar/jumbotron';
 import DropMenu from './toolbar/dropmenu';
 import NewsForm from './toolbar/newsForm';
-
+import NavMenu from './toolbar/navMenu';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+        <NavMenu />
         <Jumbotron />
         <DropMenu />
         <NewsForm />
