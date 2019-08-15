e3502796c7babf8e75eb67e8e30b7c7a0dfecedc
Khurana <39412425+Ashish0125@users.noreply.github.com>
2019-08-14 19:14:31 -0500

    cleared

--- models/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Sponsor (SfirstName, SlastName, Semail, Sphone, Scity, Scountry)
-VALUES ('Ashish', 'Singh', 'ashishkhurana17@gmail.com', 4694862840, 'Lewisville', 'USA');
-
-INSERT INTO Children (CfirstName, Cimage, ClastName, Cage, Ccity, Ccountry)
-VALUES ('Mike', "https://upload.wikimedia.org/wikipedia/en/thumb/6/6f/David_Schwimmer_as_Ross_Geller.jpg/220px-David_Schwimmer_as_Ross_Geller.jpg", 8, "Lewisville", "USA")
